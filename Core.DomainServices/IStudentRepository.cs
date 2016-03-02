6e777d4dd481c179b1d3c41efb89498762af06da
Andreas Stensig Jensen <asj@it-minds.dk>
2016-03-02 14:00:55 +0100

    Added interface and concrete of model class (Student) repository.

---
+++ Core.DomainServices/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DomainModel;
+
+namespace Core.DomainServices {
+    public interface IStudentRepository : IGenericRepository<Student> {
+        // Define any members specific for student access functionality
+
+        // E.g.
+        IEnumerable<Student> GetTenHighestGpa();
+    }
+}
---
+++ Infrastructure.DataAccess/StudentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DomainModel;
+using Core.DomainServices;
+
+namespace Infrastructure.DataAccess {
+    public class StudentRepository : GenericRepository<Student>, IStudentRepository {
+
+        public StudentRepository(ApplicationContext context) : base(context) {
+        }
+
+        public IEnumerable<Student> GetTenHighestGpa() {
+            Expression<Func<Student, bool>> filter = s => s.Id > 0;
+            Func<IQueryable<Student>, IOrderedQueryable<Student>> orderBy =
+                students => students.OrderByDescending(s => s.AverageGrade);
+
+            return Get(filter, orderBy, "", 1, 10);
+        }
+    }
+}
