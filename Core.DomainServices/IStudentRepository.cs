8307edb41fa67ed4eb82bdf5644084e19da58907
Andreas Stensig Jensen <asj@it-minds.dk>
2016-03-04 15:19:38 +0100

    Added member functions.

--- Core.DomainServices/IStudentRepository.cs
+++ Core.DomainServices/IStudentRepository.cs
@@ -6,8 +6,17 @@ namespace Core.DomainServices
     public interface IStudentRepository : IGenericRepository<Student> 
     {
         // Define any members specific for student access functionality
-
         // E.g.
+
+        Student GetStudent(int id);
+        IEnumerable<Student> GetStudents();
+        IEnumerable<Student> GetStudents(string name);
         IEnumerable<Student> GetThreeHighestGpa();
+
+        void AddStudent(Student student);
+        void AddStudents(IEnumerable<Student> students);
+
+        void DeleteStudent(int id);
+        void DeleteStudents(IEnumerable<int> ids);
     }
 }
--- Infrastructure.DataAccess/StudentRepository.cs
+++ Infrastructure.DataAccess/StudentRepository.cs
@@ -8,13 +8,81 @@ namespace Infrastructure.DataAccess
     public class StudentRepository : GenericRepository<Student>, IStudentRepository 
     {
 
+        // Handle query syntax within the repository; don't expose it 
+        // to the controllers that use the repository.
+
         public StudentRepository(ApplicationContext context) : base(context) { }
-        
+
+        /// <summary>
+        /// Find a student by ID. 
+        /// </summary>
+        /// <param name="id">The student's unique id.</param>
+        /// <returns>The found student, or null.</returns>
+        public Student GetStudent(int id) {
+            return GetByKey(id);
+        }
+
+        /// <summary>
+        /// Get all students.
+        /// </summary>
+        /// <returns>A collection of all students.</returns>
+        public IEnumerable<Student> GetStudents() {
+            return Get();
+        }
+
+        /// <summary>
+        /// Get all students with a given name.
+        /// Note that if the parameter is null, null is returned. 
+        /// </summary>
+        /// <param name="name">Name to match against.</param>
+        /// <returns>All found matches, an empty collection if none were found, or null if argument was null.</returns>
+        public IEnumerable<Student> GetStudents(string name) {
+            return name != null ? Get(filter: (s => s.Name == name)) : null;
+        }
+
+        /// <summary>
+        /// Get the three students with the highest GPA (Grade Point Average).
+        /// </summary>
+        /// <returns>A collection of (up to three) students with the highest GPA.</returns>
         public IEnumerable<Student> GetThreeHighestGpa() 
         {
-            // Handle query syntax within the repository; don't expose it 
-            // to the controllers that use the repository.
             return Get(null, students => students.OrderByDescending(student => student.AverageGrade), "", 1, 3);
+        }
+
+        /// <summary>
+        /// Add a new student.
+        /// </summary>
+        /// <param name="student">The new student.</param>
+        public void AddStudent(Student student) {
+            Insert(student);
+        }
+
+        /// <summary>
+        /// Add a number of students.
+        /// </summary>
+        /// <param name="students">Collection of new students.</param>
+        public void AddStudents(IEnumerable<Student> students) {
+            foreach (var student in students) {
+                Insert(student);
+            }
+        }
+
+        /// <summary>
+        /// Remove a student. 
+        /// </summary>
+        /// <param name="id">Unique ID of the student to be removed.</param>
+        public void DeleteStudent(int id) {
+            DeleteByKey(id);
+        }
+
+        /// <summary>
+        /// Remove a number of students.
+        /// </summary>
+        /// <param name="ids">Collection of unique IDs of the students to be removed.</param>
+        public void DeleteStudents(IEnumerable<int> ids) {
+            foreach (var id in ids) {
+                DeleteByKey(id);
+            }
         }
     }
 }
